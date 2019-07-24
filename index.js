c358b4659c974de4f0a619c398ebd03c17a01068
Nathan Buchanan <nate.buchanan@me.com>
2019-07-24 22:39:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,10 +11,8 @@ var animal = 'cat'
 }
 
 function add2(n) {
-  return n + two
-
-  // Feel free to move things around!
-  const two = 2
+var two = 2
+  return n + two;
 }
 
 var funkyFunction = function() {
